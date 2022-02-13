2e477de69c74248dd27b7d7acb4eeed4ca1d5e8f
yamaslena@yandex.ru <yamaslena@yandex.ru>
2022-02-13 10:23:00 +0700

    добавлен компонент Login

--- project/src/components/header-component/header-component.tsx
+++ project/src/components/header-component/header-component.tsx
@@ -1,4 +1,5 @@
 import Logo from '../logo-component/logo-component';
+import Login from '../login-component/login-component';
 
 function Header(): JSX.Element {
   return (
@@ -6,22 +7,7 @@ function Header(): JSX.Element {
       <div className="container">
         <div className="header__wrapper">
           <Logo />
-          <nav className="header__nav">
-            <ul className="header__nav-list">
-              <li className="header__nav-item user">
-                <a className="header__nav-link header__nav-link--profile" href="/">
-                  <div className="header__avatar-wrapper user__avatar-wrapper">
-                  </div>
-                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a className="header__nav-link" href="/">
-                  <span className="header__signout">Sign out</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
+          <Login />
         </div>
       </div>
     </header>
