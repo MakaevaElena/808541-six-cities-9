76d5a15f27fd104e2e1afe99aa5f03b2d94f76d7
yamaslena@yandex.ru <yamaslena@yandex.ru>
2022-03-11 11:02:07 +0700

    подключен axios в api.ts

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-import { offers } from './mocks/offers';
-import { reviews } from './mocks/reviews';
-import { favorites } from './mocks/favorites';
+// import { offers } from './mocks/offers';
+// import { reviews } from './mocks/reviews';
+// import { favorites } from './mocks/favorites';
 import { Provider } from 'react-redux';
 import { store } from './store';
+
+const Settings = {
+  offers: [],
+  reviews: [],
+  favorites: [],
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App
-        offers={offers}
-        reviews={reviews}
-        favorites={favorites}
+        offers={Settings.offers}
+        reviews={Settings.reviews}
+        favorites={Settings.favorites}
       />
     </Provider>
   </React.StrictMode>,
