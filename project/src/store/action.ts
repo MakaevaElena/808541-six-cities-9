286ca87853c1b0516455baf6d21d8bba510695aa
yamaslena@yandex.ru <yamaslena@yandex.ru>
2022-03-11 13:14:38 +0700

    7.7. Добавит признак наличия авторизации в глобальное состояние

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,9 +1,11 @@
 import { createAction } from '@reduxjs/toolkit';
 import { OfferType } from '../types/offer-type';
 import { FavoriteType } from '../types/favorite-type';
+import { AuthorizationStatus } from '../const';
 
 export const setCity = createAction<string>('MainScreen/setSity');
 export const setOffers = createAction<OfferType[]>('MainScreen/setOffers');
 export const setSortType = createAction<string>('MainScreen/setSortType');
 export const loadOffers = createAction<OfferType[]>('loadOffers');
 export const loadFavorite = createAction<FavoriteType[]>('data/fetchFavorite');
+export const requireAuthorization = createAction<AuthorizationStatus>('user/requireAuthorization');
--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,10 +1,11 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { setCity, setOffers, setSortType, loadOffers, loadFavorite } from './action';
+import { setCity, setOffers, setSortType, loadOffers, loadFavorite, requireAuthorization } from './action';
 // import { offers } from '../mocks/offers';
 import { CITIES } from '../const';
 import { SortingType } from '../utils';
 import { OfferType } from '../types/offer-type';
 import { FavoriteType } from '../types/favorite-type';
+import { AuthorizationStatus } from '../const';
 
 const DEFAULT_OFFERS: OfferType[] = [];
 const DEFAULT_FAVORITES: FavoriteType[] = [];
@@ -16,10 +17,14 @@ const initialState = {
   sortType: SortingType.POPULAR,
   isOffersLoaded: false,
   isFavoriteLoaded: false,
+  authorizationStatus: AuthorizationStatus.Unknown,
 };
 
 export const reducer = createReducer(initialState, (builder) => {
   builder
+    .addCase(requireAuthorization, (state, action) => {
+      state.authorizationStatus = action.payload;
+    })
     .addCase(loadOffers, (state, action) => {
       state.offers = action.payload;
       state.isOffersLoaded = true;
