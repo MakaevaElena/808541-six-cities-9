49dcb9247bdfea7ca1ef08a5063500ecea4732b4
yamaslena@yandex.ru <yamaslena@yandex.ru>
2022-03-11 11:52:04 +0700

    7.3. Добавит модуль для работы с токеном.

--- project/src/store/index.ts
+++ project/src/store/index.ts
@@ -1,4 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './reducer';
+import { createAPI } from '../services/api';
 
-export const store = configureStore({ reducer });
+// export const store = configureStore({ reducer });
+const api = createAPI();
+
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    thunk: {
+      extraArgument: api,
+    },
+  }),
+});
+
+export { store, api };
