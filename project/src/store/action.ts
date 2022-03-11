7b922b7bb9e447d23794c841a3c5097a5edd7484
yamaslena@yandex.ru <yamaslena@yandex.ru>
2022-03-11 12:34:24 +0700

    7.5. Добавит новые действия:загрузка offers и favorites

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,6 +1,9 @@
 import { createAction } from '@reduxjs/toolkit';
 import { OfferType } from '../types/offer-type';
+import { FavoriteType } from '../types/favorite-type';
 
 export const setCity = createAction<string>('MainScreen/setSity');
 export const setOffers = createAction<OfferType[]>('MainScreen/setOffers');
 export const setSortType = createAction<string>('MainScreen/setSortType');
+export const loadOffers = createAction<OfferType[]>('loadOffers');
+export const loadFavorite = createAction<FavoriteType[]>('data/fetchFavorite');
--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,17 +1,33 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { setCity, setOffers, setSortType } from './action';
-import { offers } from '../mocks/offers';
+import { setCity, setOffers, setSortType, loadOffers, loadFavorite } from './action';
+// import { offers } from '../mocks/offers';
 import { CITIES } from '../const';
 import { SortingType } from '../utils';
+import { OfferType } from '../types/offer-type';
+import { FavoriteType } from '../types/favorite-type';
+
+const DEFAULT_OFFERS: OfferType[] = [];
+const DEFAULT_FAVORITES: FavoriteType[] = [];
 
 const initialState = {
   city: CITIES[0],
-  offers: offers,
+  offers: DEFAULT_OFFERS,
+  favorites: DEFAULT_FAVORITES,
   sortType: SortingType.POPULAR,
+  isOffersLoaded: false,
+  isFavoriteLoaded: false,
 };
 
 export const reducer = createReducer(initialState, (builder) => {
   builder
+    .addCase(loadOffers, (state, action) => {
+      state.offers = action.payload;
+      state.isOffersLoaded = true;
+    })
+    .addCase(loadFavorite, (state, action) => {
+      state.favorites = action.payload;
+      state.isFavoriteLoaded = true;
+    })
     .addCase(setCity, (state, action) => {
       state.city = action.payload;
     })
