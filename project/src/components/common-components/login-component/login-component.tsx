d03c7f4092a8b0134ccdd493755c95de9d6fe6b0
yamaslena@yandex.ru <yamaslena@yandex.ru>
2022-03-12 22:21:31 +0700

    Login

--- project/src/components/common-components/login-component/login-component.tsx
+++ project/src/components/common-components/login-component/login-component.tsx
@@ -1,22 +1,42 @@
 import { Link } from 'react-router-dom';
-import { AppRoute } from '../../../const';
+import { AppRoute, AuthorizationStatus } from '../../../const';
+import { useAppSelector } from '../../../hooks';
+// import { getToken } from '../../../services/token';
 
 function Login(): JSX.Element {
+  const { authorizationStatus } = useAppSelector((state) => state);
+
   return (
     <nav className="header__nav">
       <ul className="header__nav-list">
-        <li className="header__nav-item user">
-          <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
-            <div className="header__avatar-wrapper user__avatar-wrapper">
-            </div>
-            <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-          </Link>
-        </li>
-        <li className="header__nav-item">
-          <Link className="header__nav-link" to={AppRoute.SignIn}>
-            <span className="header__signout">Sign out</span>
-          </Link>
-        </li>
+        {
+          authorizationStatus === AuthorizationStatus.Auth &&
+          <>
+            <li className="header__nav-item user">
+              <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
+                <div className="header__avatar-wrapper user__avatar-wrapper">
+
+                </div>
+                <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+              </Link>
+            </li>
+            <li className="header__nav-item">
+              <Link className="header__nav-link" to={AppRoute.Favorites}>
+                <span className="header__signout">Sign out</span>
+              </Link>
+            </li>
+          </>
+        }
+        {
+          authorizationStatus !== AuthorizationStatus.Auth &&
+          <li className="header__nav-item user">
+            <Link className="header__nav-link header__nav-link--profile" to={AppRoute.SignIn}>
+              <div className="header__avatar-wrapper user__avatar-wrapper">
+              </div>
+              <span className="header__login">Sign in</span>
+            </Link>
+          </li>
+        }
       </ul>
     </nav>
   );
--- project/src/components/login-screen-component/login-screen-component.tsx
+++ project/src/components/login-screen-component/login-screen-component.tsx
@@ -1,6 +1,33 @@
 import Header from '../common-components/header-component/header-component';
+import { FormEvent, useRef } from 'react';
+import { useAppDispatch } from '../../hooks';
+import { loginAction } from '../../store/api-action/api-actions';
+import { AuthData } from '../../types/auth-data';
+import { AppRoute } from '../../const';
+import { Link } from 'react-router-dom';
 
 function LoginScreen(): JSX.Element {
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+
+  const onSubmit = (authData: AuthData) => {
+    dispatch(loginAction(authData));
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current !== null && passwordRef.current !== null) {
+      onSubmit({
+        login: loginRef.current.value,
+        password: passwordRef.current.value,
+      });
+
+    }
+  };
+
   return (
     <div className="page page--gray page--login">
       <Header />
@@ -8,23 +35,37 @@ function LoginScreen(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form onSubmit={handleSubmit} className="login__form form" action="#" method="post">
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required />
+                <input
+                  ref={loginRef}
+                  className="login__input form__input"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  required
+                />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
+                <input
+                  ref={passwordRef}
+                  className="login__input form__input"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                />
               </div>
               <button className="login__submit form__submit button" type="submit">Sign in</button>
             </form>
           </section>
           <section className="locations locations--login locations--current">
             <div className="locations__item">
-              <a className="locations__item-link" href="/">
+              <Link className="locations__item-link" to={AppRoute.Main}>
                 <span>Amsterdam</span>
-              </a>
+              </Link>
             </div>
           </section>
         </div>
