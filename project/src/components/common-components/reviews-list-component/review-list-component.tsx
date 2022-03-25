1808bd8c8d830864c3ffc6868f903383fb64adec
yamaslena@yandex.ru <yamaslena@yandex.ru>
2022-03-25 17:05:38 +0700

    комментариев не больше 10

--- project/src/components/common-components/reviews-list-component/review-list-component.tsx
+++ project/src/components/common-components/reviews-list-component/review-list-component.tsx
@@ -6,12 +6,17 @@ type ReviewsListProps = {
   reviews: ReviewType[];
 }
 
+const MAX_COUNT_OF_REVIEWS = 10;
+
 function ReviewList({ reviews }: ReviewsListProps): JSX.Element {
+
+  const totalReviews = reviews.slice(0, MAX_COUNT_OF_REVIEWS);
+
   return (
     <>
-      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
+      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{totalReviews.length}</span></h2>
       <ul className="reviews__list">
-        {reviews.map((review) => (
+        {totalReviews.map((review) => (
           <li className="reviews__item" key={review.id}>
             <div className="reviews__user user">
               <div className="reviews__avatar-wrapper user__avatar-wrapper">
